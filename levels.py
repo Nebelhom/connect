d9220bbafce3fce8a993c1bd63e579987c5ca8b4
Nebelhom <nebelhom@hotmail.com>
2016-12-29 17:30:38 +0100

    Add levels.py, fix sizing, create first level

---
+++ levels.py
@@ -0,0 +1,16 @@
+#!usr/bin/python3
+
+"""
+Outlines the levels in form of dictionaries
+"""
+
+lvl1 = {1: {'x':0.5,
+            'y':0.66667,
+            'links':[2,3]},
+        2: {'x':0.33333,
+            'y':0.33333,
+            'links':[1,3]},
+        3: {'x':0.66667,
+            'y':0.33333,
+            'links':[1,2]},
+        }
--- main.py
+++ main.py
@@ -6,6 +6,18 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics import Color, Ellipse
 from kivy.properties import NumericProperty
 
+from levels import *
+
+"""
+Help for pos_hint
+
+coordinate always corresponds to bottom left corner of widget
+
+'x':0 -> x = 0
+'x':1 -> x = width, i.e. widget exactly outside
+'x':0.5 -> x = width / 2
+"""
+
 # Kivy colors in RGBA
 WHITE = Color(1.0, 1.0, 1.0, 1.0)
 RED = Color(1.0, 0.0, 0.0, 1.0)
@@ -13,16 +25,18 @@ GREEN = Color(0.0, 1.0, 0.0, 1.0)
 BLUE = Color(0.0, 0.0, 1.0, 1.0)
 BLACK = Color(0.0, 0.0, 0.0, 1.0)
 
-
 class Dot(Widget):
+    r = NumericProperty(0)
     def __init__(self, **kwargs):
         super(Dot, self).__init__(**kwargs)
 
         # Properties
         self.r = 1.0
-        self.size = 30, 30
+        self.size_hint = None, None
+        self.size = 30, 30  # The size (see update function)
 
         # Bindings
+        self.bind(r=self.update)
         self.bind(pos=self.update)
         self.bind(size=self.update)
 
@@ -36,30 +50,27 @@ class Dot(Widget):
                 self.r = 0.0
             else:
                 self.r = 1.0
-        # In order to take effect
-        self.update()
 
     def update(self, *args):
         """
         Updates visuals
         - Input from resizing and touch_events
         """
-        # clear canvas
         self.canvas.clear()
 
-        # Redraw canvas
         with self.canvas:
             Color(self.r, 1.0, 1.0, 1.0)
-            Ellipse(pos=self.pos, size=(30, 30))
+            Ellipse(pos=self.pos, size=self.size)
+
 
 class SchemeGame(FloatLayout):
     def __init__(self,**kwargs):
         super(SchemeGame, self).__init__(**kwargs)
 
         # Draw the ellipse widgets
-        e = Dot(pos_hint={'x': 0.5, 'y': 0.5}, size=(30,30))
-        self.add_widget(e)
-
+        for key, value in lvl1.items():
+            e = Dot(pos_hint={'x': value['x'], 'y': value['y']}, size=(30,30))
+            self.add_widget(e)
 
 class SchemeApp(App):
     def build(self):
