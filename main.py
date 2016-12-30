8291a8a90ee33301431d3522a6103c56270d652a
Nebelhom <nebelhom@hotmail.com>
2016-12-30 03:16:00 +0100

    Lines can be drawn

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
-from kivy.graphics import Color, Ellipse
-from kivy.properties import BooleanProperty, NumericProperty
+from kivy.graphics import Color, Ellipse, Line
+from kivy.properties import BooleanProperty, ListProperty, NumericProperty
 
 from levels import *
 
@@ -25,13 +25,28 @@ GREEN = Color(0.0, 1.0, 0.0, 1.0)
 BLUE = Color(0.0, 0.0, 1.0, 1.0)
 BLACK = Color(0.0, 0.0, 0.0, 1.0)
 
-class TouchException(Exception):
-    print("An expected combination of factors were observed.")
+class Ray(Widget):
+    def __init__(self, coords=[0, 0, 100, 100],
+                 **kwargs):
+        super(Ray, self).__init__(**kwargs)
+
+        self.coords = coords
+
+        # Bindings
+        self.bind(pos=self.update)
+        self.bind(size=self.update)
+
+    def update(self, *args):
+        with self.canvas:
+            Color(1.0, 1.0, 1.0, 1.0)
+            Line(points=self.coords)
 
 class Dot(Widget):
     r = NumericProperty(0)
     collision = BooleanProperty(False)
     highlight = BooleanProperty(False)
+    # Coordinates of the center of the widget
+    pos_center = ListProperty([])
     def __init__(self, **kwargs):
         super(Dot, self).__init__(**kwargs)
 
@@ -41,18 +56,26 @@ class Dot(Widget):
 
         # Dot-specific Properties
         self.r = 1.0
+        #print(self.pos)
+        self.pos_center = self.get_pos_center()
 
         # Bindings
         self.bind(pos=self.update)
         self.bind(size=self.update)
 
         self.bind(r=self.update)
+        self.bind(pos_center=self.update)
+
+    def get_pos_center(self):
+        return ((self.pos[0] + self.width / 2.0),
+                (self.pos[1] + self.height / 2.0))
 
     def update(self, *args):
         """
         Updates visuals
         - Input from resizing and touch_events
         """
+        self.pos_center = self.get_pos_center()
 
         # Redraw
         self.canvas.clear()
@@ -61,19 +84,36 @@ class Dot(Widget):
             Color(self.r, 1.0, 1.0, 1.0)
             Ellipse(pos=self.pos, size=self.size)
 
-
 class SchemeGame(FloatLayout):
     def __init__(self,**kwargs):
         super(SchemeGame, self).__init__(**kwargs)
 
+        # Pos of previously active sphere
+        self.prev_pos = (-1, -1)
+        # Pos of currently active sphere
+        self.cur_pos = (-1, -1)
+
         # Managing dots
         self.dots = []
+        self.draw_ellipses()
 
+
+    def draw_ellipses(self):
         # Draw the ellipse widgets
         for key, value in lvl1.items():
             e = Dot(pos_hint={'x': value['x'], 'y': value['y']}, size=(30,30))
             self.dots.append(e)
             self.add_widget(e)
+
+    def draw_line(self, start, end):
+        """
+        Draws straight line between two dots
+        start = tuple
+        end = tuple
+        """
+        ray = Ray(coords=[start[0], start[1],
+                          end[0], end[1]])
+        self.add_widget(ray)
 
     def on_touch_down(self, touch):
         """
@@ -105,16 +145,24 @@ class SchemeGame(FloatLayout):
             if dot.collision and not dot.highlight:
                 dot.r = 0.0
                 dot.highlight = True
+                # Save new current position
+                self.cur_pos = dot.pos_center
+
             # Check 3
             elif dot.collision and dot.highlight:
                 pass
+
             # Check 4
             elif collision and not dot.collision and dot.highlight:
                 dot.r = 1.0
                 dot.highlight = False
+                # Move previous active position
+                self.prev_pos = dot.pos_center
 
+        # Here we draw a new line
+        if self.cur_pos != (-1, -1) and self.prev_pos != (-1, -1):
 
-
+            self.draw_line(self.prev_pos, self.cur_pos)
 
 class SchemeApp(App):
     def build(self):
